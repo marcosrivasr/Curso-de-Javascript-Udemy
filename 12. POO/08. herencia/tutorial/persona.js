20fba8963fb721ce99b759d6b5aa40b98ee14d89
Marcos Rivas Rojas <marcosrivasr@gmail.com>
2020-12-22 20:12:08 -0600

    archivos de herencia y encadenamiento opcional

--- 12. POO/08. herencia/tutorial/persona.js
+++ 12. POO/08. herencia/tutorial/persona.js
@@ -1,22 +1,2 @@
 class Persona{
-
-    #nombre;
-    #edad;
-
-    constructor(nombre, edad){
-        this.#nombre = nombre;
-        this.#edad = edad;
-    }
-
-    presentarse(){
-        return `mi nombre es ${this.#nombre} y tengo ${this.#edad} años`;
-    }
-
-    set nombre(value){
-        this.#nombre = value;
-    }
-
-    get nombre(){
-        return this.#nombre;
-    }
 }
